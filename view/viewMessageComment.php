a18801f90c97d7383c957cb497c20f6b55b0cf89
Romain <romain.oclassrooms@gmail.com>
2019-08-23 16:31:04 +0200

    add message

---
+++ view/viewMessageComment.php
@@ -0,0 +1,22 @@
+<?php $this->header = "Commentaire" ?>
+
+<!-- Page blog header -->
+<section class="page-section">
+    <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">En attente de validation</h2>
+
+    <div class="divider-custom">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+            <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+    </div>
+
+    <div class="container alert alert-success">
+        Votre commentaire est en attente de validation
+    </div>
+    <div class="container">
+        <a class="btn btn-primary" href="index.php?p=listpost">Retour</a>
+    </div>
+
+</section>
