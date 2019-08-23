014e11286aa55412f8ec85c3dfba14719e67114f
Romain <romain.oclassrooms@gmail.com>
2019-08-23 15:49:51 +0200

    view user administration

---
+++ view/viewAccesDenied.php
@@ -0,0 +1,20 @@
+<!-- Page blog header -->
+<?php $this->header = "Accès refusé" ?>
+
+<section class="page-section">
+  <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Vous n'êtes pas autorisé à accéder à ce lien</h2>
+
+  <div class="divider-custom">
+    <div class="divider-custom-line"></div>
+      <div class="divider-custom-icon">
+        <i class="fas fa-star"></i>
+      </div>
+    <div class="divider-custom-line"></div>
+  </div>
+
+  <div class="container">
+  <a class="btn btn-success" href="index.php?u=login">Se connecter</a>
+  <a class="btn btn-primary" href="index.php">Retour</a>
+  </div>
+
+</section>
