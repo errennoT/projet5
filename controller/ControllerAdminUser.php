da9196377a235c3e344d34513b0b4eea49afc5d9
Romain <romain.oclassrooms@gmail.com>
2019-08-23 15:51:55 +0200

    Delete user

---
+++ controller/ControllerAdminUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Projet5\Controller;
+
+use Projet5\Model\User;
+use Projet5\Model\UserManager;
+use Projet5\Model\AreaAdmin;
+use Projet5\Service\GenerateView;
+
+class ControllerAdminUser extends GenerateView
+{
+    private $_userManager;
+    private $areaAdmin;
+
+    public function __construct()
+    {
+        $this->_userManager = new UserManager();
+    }
+
+    //Liste des utilisateurs
+    public function listUser()
+    {
+        $areaAdmin = $this->areaAdmin = new AreaAdmin;
+        $areaAdmin->verifyAdmin();
+
+        $users = $this->_userManager->getList();
+
+        $this->generateView("AdminUser", null, 'users', $users);
+    }
+
+    //Supprimer un utilisateur
+    public function delete($id)
+    {
+        $areaAdmin = $this->areaAdmin = new AreaAdmin;
+        $areaAdmin->verifyAdmin();
+
+        $userId = $this->_userManager->get($id);
+        $this->_userManager->delete($userId);
+        header('Location: index.php?u=adminuser#list');
+    }
+
+}
