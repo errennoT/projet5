7a0727e4041bae3d022e6a089c4e6b6d69b991af
Romain <romain.oclassrooms@gmail.com>
2019-08-23 16:14:47 +0200

    CommentManager class

---
+++ model/CommentManager.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Projet5\Model;
+
+use \Projet5\Model\Comment;
+use \Projet5\Model\PostManager;
+use \Projet5\View\View;
+use \Projet5\Service\DataBase;
+use \PDO;
+
+class CommentManager extends DataBase
+{
+
+    private $postManager;
+
+    public function add(Comment $comment)
+    {
+        $q = $this->dbConnect()->prepare('INSERT INTO comment(postid, author, content, date, status) VALUES (:postid, :author, :content, NOW(), :status)');
+
+        $q->bindValue(':postid', $comment->postId(), PDO::PARAM_INT);
+        $q->bindValue(':author', $comment->author(), PDO::PARAM_STR);
+        $q->bindValue(':content', $comment->content(), PDO::PARAM_STR);
+        $q->bindValue(':status', $comment->status(), PDO::PARAM_BOOL);
+
+        $this->executeSql($q);
+    }
+
+    public function delete(Comment $comment)
+    {
+        $q = $this->dbConnect()->prepare('DELETE FROM comment WHERE id = :id');
+        $q->bindValue(':id', $comment->id(), PDO::PARAM_INT);
+
+        $this->executeSql($q);
+    }
+
+    public function deleteCommentInPost($postId)
+    {
+        $q = $this->dbConnect()->prepare('DELETE FROM comment WHERE postid = :postid');
+        $q->bindValue(':postid', $postId, PDO::PARAM_INT);
+
+        $this->executeSql($q);
+    }
+
+    public function validate(Comment $comment)
+    {
+        $q = $this->dbConnect()->prepare('UPDATE comment SET status = 0 WHERE id = :id');
+        $q->bindValue(':id', $comment->id(), PDO::PARAM_INT);
+
+        $this->executeSql($q);
+    }
+
+    public function unvalidate(Comment $comment)
+    {
+        $q = $this->dbConnect()->prepare('UPDATE comment SET status = 1 WHERE id = :id');
+        $q->bindValue(':id', $comment->id(), PDO::PARAM_INT);
+
+        $this->executeSql($q);
+    }
+
+    public function get($id)
+    {
+        $id = (int) $id;
+
+        $q = $this->dbConnect()->prepare('SELECT id, postid, author, content, date, status FROM comment WHERE id = ' . $id);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+
+        return new Comment($data);
+    }
+
+    public function getList($status, $postId = null)
+    {
+        $comment = [];
+
+        switch ($status) {
+            case "admin":
+                $q = $this->dbConnect()->prepare('SELECT id, postid, author, content, date, status FROM comment ORDER BY id desc');
+                break;
+            case "adminmanager":
+                $q = $this->dbConnect()->prepare('SELECT id, postid, author, content, date, status FROM comment WHERE status = 0 ORDER BY id desc');
+                break;
+            case "user":
+                $q = $this->dbConnect()->prepare("SELECT id, postid, author, content, date, status FROM comment WHERE postid = $postId && status = 1 ORDER BY id desc");
+                break;
+        }
+
+        $q->execute(array());
+        while ($data = $q->fetch(PDO::FETCH_ASSOC)) {
+            $comment[] = new Comment($data);
+        }
+
+        return $comment;
+    }
+
+    //Envoie une erreur si le formulaire est mal rempli
+    public function errorComment($content, $postId)
+    {
+        $error = [];
+        if (strlen($content) < 5) {
+            $error['content'] = 'Votre commentaire est trop court';
+            $postManager = $this->postManager = new PostManager();
+            $post = $postManager->get($postId);
+
+            $comments = $this->getList("user", $postId);
+
+            $view = new View("Post", $error);
+            $view->generate(array('post' => $post, 'comments' => $comments));
+        }
+
+        return $error;
+    }
+
+    //Vérifie les données
+    public function sendComment($content, $status, $postId, $author, $id = null)
+    {
+        $data['content'] = $content;
+        $data['postId'] = $postId;
+        $data['author'] = $author;
+        $data['id'] = $id;
+
+        switch ($status) {
+            case true:
+                $data['status'] = 1;
+                break;
+            case false:
+                $data['status'] = 0;
+                break;
+        }
+
+        return $data;
+    }
+}
